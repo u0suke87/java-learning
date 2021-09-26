326b9915d2c4517e9c9451226767af4431f62138
中山雄介 <yusuke@nakayamayuusukenoMacBook-Pro.local>
2021-09-26 15:25:32 +0900

    Implement singleton

--- greet/src/main/java/demo/Main.java
+++ greet/src/main/java/demo/Main.java
@@ -3,18 +3,20 @@ package demo;
 import entity.Member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import service.MemberService;
 import service.MemberServiceImpl;
 
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        MemberServiceImpl service = new MemberServiceImpl();
+//        MemberServiceImpl service = new MemberServiceImpl();
+        MemberService service = MemberServiceImpl.getInstance();
         System.out.println(service.greet(2));
         System.out.println(service.getAll());
 
         ArrayList<Member> list = service.getAll();
-        for(Member mem : list){
+        for (Member mem : list) {
             System.out.println(mem.getId() + "," + mem.getName() + "," + mem.getEmail());
         }
     }
